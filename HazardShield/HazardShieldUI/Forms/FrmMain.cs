1ad15cb907142dcc169ed86350a53fba68b10c80
xqrzd <andy@orbitech.org>
2015-01-09 18:43:00 -0500

    Added tab control by http://homepage.ntlworld.com/mdaudi100/alternate/tabcontrols.html

--- HazardShield/HazardShieldUI/Forms/FrmMain.cs
+++ HazardShield/HazardShieldUI/Forms/FrmMain.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+*  Copyright (C) 2015 Orbitech
+*
+*  Authors: xqrzd
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License version 2 as
+*  published by the Free Software Foundation.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+*  MA 02110-1301, USA.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +36,6 @@ namespace HazardShieldUI.Forms
         {
             InitializeComponent();
         }
+
     }
 }
---
+++ HazardShield/HazardShieldUI/Forms/HiddenTabControl.cs
@@ -0,0 +1,41 @@
+﻿// Author: http://homepage.ntlworld.com/mdaudi100/alternate/tabcontrols.html
+
+using System.ComponentModel;
+using System.Drawing;
+
+namespace HazardShieldUI.Forms
+{
+    [ToolboxBitmap(typeof(System.Windows.Forms.TabControl))]
+    public class HiddenTabControl : System.Windows.Forms.TabControl
+    {
+        private bool m_HideTabs = false;
+
+        [DefaultValue(false)]
+        [RefreshProperties(RefreshProperties.All)]
+        public bool HideTabs
+        {
+            get { return m_HideTabs; }
+            set
+            {
+                if (m_HideTabs == value)
+                    return;
+
+                m_HideTabs = value;
+
+                this.UpdateStyles();
+            }
+        }
+
+        public override System.Drawing.Rectangle DisplayRectangle
+        {
+            get
+            {
+                if (this.HideTabs)
+                    return new Rectangle(0, 0, Width, Height);
+                else
+                    return base.DisplayRectangle;
+            }
+        }
+
+    }
+}
