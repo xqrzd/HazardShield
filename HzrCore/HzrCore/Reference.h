b3998df3f77d0eb86c9e0d1468f691f9cb616217
xqrzd <andy@orbitech.org>
2015-03-21 22:01:29 -0400

    [HzrCore] Add comments to Reference.h

--- HzrCore/HzrCore/Reference.h
+++ HzrCore/HzrCore/Reference.h
@@ -41,28 +41,57 @@ typedef VOID(*PHS_TYPE_DELETE_PROCEDURE)(
 	);
 
 typedef struct _HS_OBJECT_TYPE {
+	/// <summary>
+	/// An optional function called when objects of this type are freed.
+	/// </summary>
 	PHS_TYPE_DELETE_PROCEDURE DeleteProcedure;
 } HS_OBJECT_TYPE, *PHS_OBJECT_TYPE;
 
 typedef struct _HS_OBJECT_HEADER {
+	/// <summary>
+	/// The reference count for this object.
+	/// </summary>
 	LONG ReferenceCount;
+
+	/// <summary>
+	/// The type of object.
+	/// </summary>
 	PHS_OBJECT_TYPE Type;
 } HS_OBJECT_HEADER, *PHS_OBJECT_HEADER;
 
+/// <summary>
+/// Creates an object managed by the reference counting system.
+/// </summary>
+/// <param name="ObjectSize">The size of the object.</param>
+/// <param name="ObjectType">The type of object.</param>
 __declspec(dllexport) PVOID HsCreateObject(
 	_In_ SIZE_T ObjectSize,
 	_In_ PHS_OBJECT_TYPE ObjectType
 	);
 
+/// <summary>
+/// Initializes an object type structure. This must be called for every
+/// object type used in the reference counting system.
+/// </summary>
+/// <param name="ObjectType">A pointer to an object type.</param>
+/// <param name="DeleteProcedure">An optional delete procedure, which is be called when an object of this type is deleted.</param>
 __declspec(dllexport) VOID HsInitializeObjectType(
 	_Out_ PHS_OBJECT_TYPE ObjectType,
 	_In_opt_ PHS_TYPE_DELETE_PROCEDURE DeleteProcedure
 	);
 
+/// <summary>
+/// Increments an object's reference count by 1.
+/// </summary>
+/// <param name="Object">A pointer to an object.</param>
 __declspec(dllexport) VOID HsReferenceObject(
 	_In_ PVOID Object
 	);
 
+/// <summary>
+/// Decrements an object's reference count by 1.
+/// </summary>
+/// <param name="Object">A pointer to an object.</param>
 __declspec(dllexport) VOID HsDereferenceObject(
 	_In_ PVOID Object
 	);
